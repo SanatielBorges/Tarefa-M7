0c04fb0f6e6fc73721377c69c0e617fd22bcc492
SanatielBorges <sanatielborges21@gmail.com>
2023-06-01 22:42:39 -0300

    Inclusão dos arquivos .css e .js
    Foram inclusos os arquivos css e js conforme solicitados pelo tutor.

---
+++ script.js
@@ -0,0 +1,16 @@
+function validarFormulario() {
+    var campoA = parseInt(document.getElementById('campoA').value);
+    var campoB = parseInt(document.getElementById('campoB').value);
+    var mensagem = document.getElementById('mensagem');
+  
+    if (campoB > campoA) {
+      mensagem.innerHTML = 'Formulário válido! Número B é maior que o número A.';
+      mensagem.classList.add('green');
+      mensagem.classList.remove('red');
+    } else {
+      mensagem.innerHTML = 'Formulário inválido! Número B deve ser maior que o número A.';
+      mensagem.classList.add('red');
+      mensagem.classList.remove('green');
+    }
+  }
+  
