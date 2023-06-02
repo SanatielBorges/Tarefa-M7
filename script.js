2f245b24234327195180fb2ec4c34760a6ecd757
SanatielBorges <sanatielborges21@gmail.com>
2023-06-01 22:50:05 -0300

    Alteração no JS
    Foi feita uma alteração no JS, pois o formulário não estava sendo limpado quando enviado.
    Com a alteração os campos do formulário serão limpos após ser enviado.

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 function validarFormulario() {
-    var campoA = parseInt(document.getElementById('campoA').value);
-    var campoB = parseInt(document.getElementById('campoB').value);
-    var mensagem = document.getElementById('mensagem');
-  
-    if (campoB > campoA) {
-      mensagem.innerHTML = 'Formulário válido! Número B é maior que o número A.';
-      mensagem.classList.add('green');
-      mensagem.classList.remove('red');
-    } else {
-      mensagem.innerHTML = 'Formulário inválido! Número B deve ser maior que o número A.';
-      mensagem.classList.add('red');
-      mensagem.classList.remove('green');
-    }
-  }
+var campoA = parseInt(document.getElementById('campoA').value);
+var campoB = parseInt(document.getElementById('campoB').value);
+var mensagem = document.getElementById('mensagem');
+
+if (campoB > campoA) {
+    mensagem.innerHTML = 'Formulário válido! Número B é maior que o número A.';
+    mensagem.classList.add('green');
+    mensagem.classList.remove('red');
+} else {
+    mensagem.innerHTML = 'Formulário inválido! Número B deve ser maior que o número A.';
+    mensagem.classList.add('red');
+    mensagem.classList.remove('green');
+}
+
+// Limpar os campos do formulário
+document.getElementById('campoA').value = '';
+document.getElementById('campoB').value = '';
+}
   
